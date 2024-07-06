9471133e6393cbd50c4c7f811d5b38c5f0fa7969
donmutalib <backendkolawole@gmail.com>
2024-07-06 15:40:16 +0100

    basic express server

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('E-Commerce API');
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+
+const start = async () => {
+    try {
+        
+        app.listen(PORT, ()=> console.log(`Server is listening on ${PORT}...`))
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+start()
