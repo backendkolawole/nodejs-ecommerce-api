334749a3f96f36b7a3bdafd8ac8c80090e2fbd2e
donmutalib <backendkolawole@gmail.com>
2024-07-06 20:40:07 +0100

    basic routes

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const connectDB = require('./config/connect')
+require("dotenv").config();
+require("express-async-errors");
 
-const PORT = process.env.PORT || 5000
+//express
+const express = require("express");
+const app = express();
 
+// database
+const connectDB = require("./config/connect");
+
+//middleware
+const errorHandlerMiddleware = require("./middleware/error-handler");
+const notFoundMiddleware = require("./middleware/not-found");
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.get("", (req, res) => {
+  res.send(`e-commerce api`);
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const start = async () => {
-    try {
-        connectDB(process.env.MONGO_URI)
-        app.listen(PORT, ()=> console.log(`Server is listening on ${PORT}...`))
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    connectDB(process.env.MONGO_URI);
+    app.listen(PORT, () => console.log(`Server is listening on ${PORT}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-}
-
-start()
+start();
