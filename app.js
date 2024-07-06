bf7039d096abed190691e9769105354c129a0473
donmutalib <backendkolawole@gmail.com>
2024-07-06 21:20:20 +0100

    morgan package

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 require("express-async-errors");
 
+const morgan = require('morgan')
+
 //express
 const express = require("express");
 const app = express();
@@ -14,6 +16,7 @@ const notFoundMiddleware = require("./middleware/not-found");
 
 const PORT = process.env.PORT || 5000;
 
+app.use(morgan('tiny'))
 app.use(express.json());
 app.get("", (req, res) => {
   res.send(`e-commerce api`);
