b1aa2ee18732aa5377ecd6f004abfeeb8b2e1eaf
donmutalib <backendkolawole@gmail.com>
2024-07-06 20:22:36 +0100

    connect to the db

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const connectDB = require('./config/connect')
 
 const PORT = process.env.PORT || 5000
 
 
 const start = async () => {
     try {
-        
+        connectDB(process.env.MONGO_URI)
         app.listen(PORT, ()=> console.log(`Server is listening on ${PORT}...`))
     } catch (error) {
         console.log(error)
--- config/connect.js
+++ config/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = (url) => {
-  return mongoose.connect(url);
+  return mongoose.connect(url).then(()=> console.log(`Connected to the DB`))
 };
 
 module.exports = connectDB;
