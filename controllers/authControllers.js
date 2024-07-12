b6d331a6033dab41c7f3ff7d923421edb282a28e
donmutalib <backendkolawole@gmail.com>
2024-07-12 10:40:54 +0100

    login and logout routes

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -16,17 +16,42 @@ const register = async (req, res) => {
 
   const user = await User.create({ name, email, password, role });
   const tokenUser = { name: user.name, userId: user._id, role: user.role };
-  attachCookieToResponse(res, tokenUser)
-  
+  attachCookieToResponse(res, tokenUser);
+
   return res.status(201).json({ user: tokenUser });
 };
 
 const login = async (req, res) => {
-  return res.send("login user");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new CustomAPIError.BadRequestError(
+      `Please enter values for email and password`
+    );
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new CustomAPIError.UnauthenticatedError(`Invalid credentials`);
+  }
+
+  const isCorrect = await user.comparePasswords(password);
+  if (!isCorrect) {
+    throw new CustomAPIError.UnauthenticatedError(`Invalid credentials`);
+  }
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  attachCookieToResponse(res, tokenUser);
+
+  return res.status(201).json({ user: tokenUser });
 };
 
 const logout = async (req, res) => {
-  return res.send("logout user");
+  res.cookie("token", "", {
+    httpOnly: true,
+    expires: new Date(Date.now() + 5 * 1000),
+  });
+  return res.status(200).json({ msg: `User logged out` });
 };
 
 module.exports = { register, login, logout };
--- models/User.js
+++ models/User.js
@@ -1,42 +1,42 @@
-const mongoose = require('mongoose')
-const validator = require('validator');
-const bcrypt = require('bcryptjs')
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String, 
-        required: [true, 'Please enter value for name field'],
+  name: {
+    type: String,
+    required: [true, "Please enter value for name field"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a value for password field"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter value for email field"],
+    validate: {
+      validator: validator.isEmail,
     },
-    password: {
-        type: String, 
-        required: [true, 'Please enter a value for password field'],
-        minlength: 3,
-        maxlength: 50
-    },
-    email: {
-        type: String, 
-        required: [true, 'Please enter value for email field'],
-        validate: {
-            validator: validator.isEmail
-        },
-        unique: true
-    },
-    role: {
-        type: String,
-        enum: ['user', 'admin']
-    }
-})
+    unique: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+  },
+});
 
-userSchema.pre('save', async function () {
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-})
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 userSchema.methods.comparePasswords = async function (password) {
-    const isMatch = await bcrypt.compare(password, this.password)
-    return isMatch
-}
+  const isMatch = await bcrypt.compare(password, this.password);
+  return isMatch;
+};
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
